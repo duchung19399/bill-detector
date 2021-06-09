cf298d1dbc0300536ead2e604972697a1cb70c4b
dat15021999 <nguyenthanhdat150299@gmail.com>
2021-06-10 00:52:51 +0700

    thay doi mau va button UI

--- BillDetector/frontend/src/components/Result/Result.js
+++ BillDetector/frontend/src/components/Result/Result.js
@@ -3,8 +3,6 @@ import { Grid } from '@material-ui/core'
 import ImageDisplay from '../ImageDisplay/ImageDisplay'
 import ImageContext from '../../contexts/ImageContext'
 
-let _url = "https://neilpatel.com/wp-content/uploads/2017/09/image-editing-tools.jpg"
-
 export default class Result extends Component {
     constructor(props) {
         super(props)
@@ -17,28 +15,40 @@ export default class Result extends Component {
                 {context => 
                     <div className="side-frame">
                         <div className="header" style={Styles.headerLabel}>
-                            <label>
-                                Image Information
-                            </label>
+                            <label>Image Information</label>
                         </div>
                         <div className="body">
-                            <div className="label-name">Tên Model</div>
-                                <ImageDisplay src={context.src} />
+                            <div className="label-name">
+                                <label>Model Name</label>
+                            </div>
+                            <ImageDisplay src={context.src} />
                                 
                             <div className="label-name">{context.name}</div>
                             <Grid style={Styles.resultFrame} container spacing={1}>
-                                <Grid item xs={5} align='right'>feature</Grid>
-                                <Grid item xs={2} align='center'>:</Grid>
-                                <Grid item xs={5} align='left'>weight</Grid>
-                                <Grid item xs={5} align='right'>feature</Grid>
-                                <Grid item xs={2} align='center'>:</Grid>
-                                <Grid item xs={5} align='left'>weight</Grid>
+                                <Grid item xs={5} align='right'>
+                                    <label style={Styles.item}>feature</label>
+                                </Grid>
+                                <Grid item xs={2} align='center'>
+                                    <label style={Styles.item}>:</label>
+                                </Grid>
+                                <Grid item xs={5} align='left'>
+                                    <label style={Styles.item}>weight</label>
+                                </Grid>
+                                <Grid item xs={5} align='right'>
+                                    <label style={Styles.item}>feature</label>
+                                </Grid>
+                                <Grid item xs={2} align='center'>
+                                    <label style={Styles.item}>:</label>
+                                </Grid>
+                                <Grid item xs={5} align='left'>
+                                    <label style={Styles.item}>weight</label>
+                                </Grid>
                             </Grid>
                         </div>
                         <div className="footer">
                             <Grid container spacing={1}>
                                 <Grid item xs={12} align='center' style={Styles.gridItem}>
-                                    <div className="button">
+                                    <div className="button" data-tip="Save Result">
                                         <label>Save Result</label>
                                     </div>
                                 </Grid>
@@ -64,5 +74,9 @@ const Styles = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+
+    item: {
+        fontSize: '15px',
     }
 }
